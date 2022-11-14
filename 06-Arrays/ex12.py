2ac7d7d1ee05f74d2c3a3919c31d54c6467d04e8
s-121-14 <s-121-14@gumiforest.ae.krakow.pl>
2022-11-14 12:57:05 +0100

    tablice

--- 06-Arrays/ex12.py
+++ 06-Arrays/ex12.py
@@ -6,15 +6,20 @@ print("number of columns", len(arr[0]))
 print(len(arr), "x", len(arr[0]))
 print(arr[0][1])
 print(arr[1][2])
+
 print()
+
 for i in range(0, len(arr[1])):
     print(arr[1][i], end = " ")
+
 print()
 print()
+
 for i in range(0, len(arr)):
     for j in range(0, len(arr[i])):
         print(arr[i][j], end= " ")
     print()
+
 print()
 for i in range(0, len(arr)):
     print(arr[i][-1])
